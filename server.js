1d3bff53126e596c8071481ca56dd5d11a36955c
udofia2 <enalsy22@gmail.com>
2019-09-22 15:33:45 +0100

    second commit

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
+require('dotenv').config()
 if(process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
 }
 const express = require('express');
 const path = require('path')
-const app = express();
+const mongoose = require('mongoose')
 const expresslayouts = require('express-ejs-layouts');
+
+const app = express();
+
+mongoose.Promise = global.Promise
+mongoose.connect(process.env.MONGO_URL, { useUnifiedTopology: true, useNewUrlParser: true });
+
+const db = mongoose.connection
+db.on('error', err=> console.error(err));
+db.once('open', ()=> console.log(`Conected to Mongoose`))
+
 const indexRouter = require('./routes/index');
 
 
@@ -14,16 +24,12 @@ app.set('layout', 'layouts/layout');
 app.use(expresslayouts)
 app.use(express.static('public'))
 
-const mongoose = require('mongoose')
 
-mongoose.connect(process.env.DATABASE_URL, { useUnifiedTopology: true, useNewUrlParser: true });
-
-const db = mongoose.connection
-db.on('error', err=> console.error(err));
-db.once('open', ()=> console.log(`Conected to Mongoose`))
 
 app.use('/', indexRouter)
 
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, ()=> console.log(`Server is started on port ${PORT}`))
+
+console.log('Connection url => ', process.env.MONGO_URL);
