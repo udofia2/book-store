d70644fe24f7ec4d554b02dccf025a964aa442f0
udofia2 <enalsy22@gmail.com>
2019-09-23 20:36:53 +0100

    more commits

--- server.js
+++ server.js
@@ -3,17 +3,19 @@ const express = require('express');
 const path = require('path')
 const mongoose = require('mongoose')
 const expresslayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 mongoose.Promise = global.Promise
 mongoose.connect(process.env.MONGO_URL, { useUnifiedTopology: true, useNewUrlParser: true });
 
-const db = mongoose.connection
-db.on('error', err=> console.error(err));
-db.once('open', ()=> console.log(`Conected to Mongoose`))
+// const db = mongoose.connection
+// db.on('error', err=> console.error(err));
+// db.once('open', ()=> console.log(`Conected to Mongoose`))
 
 const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/authors')
 
 
 app.set('view engine', 'ejs')
@@ -21,10 +23,11 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('layout', 'layouts/layout');
 app.use(expresslayouts)
 app.use(express.static('public'))
-
+app.use(bodyParser.urlencoded({limit: '10mb', extended: false}))
 
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 const PORT = process.env.PORT || 3000
 
